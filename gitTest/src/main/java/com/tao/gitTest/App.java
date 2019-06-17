c96b4b8929a3ed760fc1a51ccee72f6dc845aa36
lixintao <lixintao03@163.com>
2019-06-17 10:44:43 +0800

    add complain

--- gitTest/src/main/java/com/tao/gitTest/App.java
+++ gitTest/src/main/java/com/tao/gitTest/App.java
@@ -1,7 +1,10 @@
 package com.tao.gitTest;
 
+
 /**
- * Hello world!
+ * Description : 
+ * Author：        李新涛                
+ * Create Date：   2019年6月17日 上午9:39:05
  *
  */
 public class App 
@@ -9,5 +12,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Hello Git!" );
     }
 }
